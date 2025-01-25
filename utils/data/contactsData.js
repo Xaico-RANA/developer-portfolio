3c38f8067815f05ab278189ddfc3d35e5194745d
Mohammad RANA <ruhulaminrana98@gmail.com>
2025-01-26 00:08:47 +0600

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -5,7 +5,7 @@ export const contactsData = {
     github: 'https://github.com/Xaico-RANA',
     facebook: 'https://www.facebook.com/XAICO.RANA',
     linkedIn: 'https://bd.linkedin.com/in/mohammad-rana-b007b8325',
-    teligram: 't.me/@KING_RANA_404',
+    telegram : 't.me/KING_RANA_404',
     stackOverflow: 'https://stackoverflow.com/username',
     devUsername: "XAICO.RANA"
 }
