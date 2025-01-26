9034bd74d108c1ed432ea6043636f9880aa00b02
Mohammad RANA <ruhulaminrana98@gmail.com>
2025-01-26 08:44:21 +0600

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,20 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
+    title: 'Cyber Security Specialist',
+    company: "All Social Platform",
     duration: "(Jan 2022 - Present)"
   },
   {
     id: 2,
-    title: "FullStack Developer",
-    company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
+    title: "FullStack Creator",
+    company: "Mail,FB,IG,X,TT ect. (freelancer)",
+    duration: "(Jun 2024 - Present)"
   },
   {
     id: 3,
     title: "Self Employed",
     company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
+    duration: "(Jan 2024 - Present)"
   }
 ]
